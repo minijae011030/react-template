11453ed285c01f90493102d7b2d4a256b8c681bf
Ian <105128049+minijae011030@users.noreply.github.com>
2025-09-27 02:18:30 +0900

    Feat: App.tsx 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 
-import App from "./app/App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
